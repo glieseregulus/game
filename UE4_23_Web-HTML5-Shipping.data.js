3169db0b540cdb9b545e2c9c2fcad27d6eb6dc97
glieseregulus <dcrss563@gmail.com>
2022-06-22 14:44:52 +0900

    6/22update

--- UE4_23_Web-HTML5-Shipping.data.js
+++ UE4_23_Web-HTML5-Shipping.data.js
@@ -173,6 +173,6 @@ Module['FS_createPath']('/UE4_23_Web/Content', 'Paks', true, true);
   }
 
  }
- loadPackage({"files": [{"start": 0, "audio": 0, "end": 52, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 52, "audio": 0, "end": 92, "filename": "/UE4CommandLine.txt"}, {"start": 92, "audio": 0, "end": 25710142, "filename": "/UE4_23_Web/Content/Paks/UE4_23_Web-HTML5.pak"}], "remote_package_size": 25710142, "package_uuid": "29892e92-e7a3-4cb6-a856-401894ca383f"});
+ loadPackage({"files": [{"start": 0, "audio": 0, "end": 52, "filename": "/Manifest_NonUFSFiles_HTML5.txt"}, {"start": 52, "audio": 0, "end": 92, "filename": "/UE4CommandLine.txt"}, {"start": 92, "audio": 0, "end": 25476439, "filename": "/UE4_23_Web/Content/Paks/UE4_23_Web-HTML5.pak"}], "remote_package_size": 25476439, "package_uuid": "245b5a28-4b75-4e84-9d15-98529e416260"});
 
 })();
